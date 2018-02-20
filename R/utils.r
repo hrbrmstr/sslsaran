059dce4fb35e18596642216e05895ca3e18f9e6c
boB Rudis <bob@rud.is>
2018-02-20 12:51:43 -0500

    Added sslmate, Symantec and Google API functions

--- R/utils.r
+++ R/utils.r
@@ -7,10 +7,13 @@
 #' @return list, data frame or character vector depending on `as`
 #' @export
 #' @examples
-#' parse_x509_attributes(
-#'   "CN=Sample Cert, OU=R&D, O=Company Ltd., L=Dublin 4, S=Dublin, C=IE"
-#' )
+#' x5_attr <- "CN=Sample Cert, OU=R&D, O=Company Ltd., L=Dublin 4, S=Dublin, C=IE"
+#' parse_x509_attributes(x5_attr)
+#' parse_x509_attributes(x5_attr, "df")
+#' parse_x509_attributes(x5_attr, "char")
 parse_x509_attributes <- function(x, as=c("list", "df", "char")) {
+
+  as <- match.arg(trimws(tolower(as)), c("list", "df", "char"))
 
   x <- trimws(stringi::stri_split_regex(x, "(?<!\\\\),")[[1]])
   x <- stringi::stri_split_fixed(x, "=", 2, simplify=TRUE)
